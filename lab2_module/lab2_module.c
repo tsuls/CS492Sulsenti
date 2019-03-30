3d8371a13721821cc6a2593b7505cc5023ab00e0
tsuls <tsulsenti60@gmail.com>
2019-03-29 22:22:02 -0400

    Added RB Tree

--- lab2_module/lab2_module.c
+++ lab2_module/lab2_module.c
@@ -6,11 +6,12 @@
 #include <linux/slab.h>
 #include <linux/random.h>
 #include <linux/list.h>
+#include <linux/rbtree.h>
 #define UPPER_BOUND 1000
 #define LOWER_BOUND 0
 #define TRUE 1
 #define FALSE 0
-#define UPPER_TREE_BOUND 50
+
 void deconstructMap(void);
 void constructLinkedList(void);
 void printLinkedList(void);
@@ -20,6 +21,10 @@ void constructQueue(void);
 int insertIntoMap(int toAdd);
 void displayMap(void);
 void initializeMap(void);
+void constructTree(void);
+void diplayTree(struct rb_root *treeRoot);
+void deconstructTree(struct rb_node *treeRoot);
+void removeRangeFromTree(struct rb_root *treeRoot);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tyler Sulsenti");
@@ -45,8 +50,7 @@ struct Map
 struct treeNode
 {
 	int data;
-	tree Node *right;
-	tree Node *left;
+	struct rb_node node;
 };
 
 struct Node *LLhead;
@@ -59,27 +63,29 @@ struct Node *QnewNode;
 
 struct Map *myMap;
 
-struct treeNode *treeHead;
+struct rb_root treeHead;
 
 int *listData;
 int *queueData;
 int *mapData;
 int *treeData;
+int *rangeData;
 
 static int dstruct_size = 2;
 module_param(dstruct_size, int,  S_IRUSR | S_IWUSR);
-MODULE_PARM_DESC(dstruct_size,"this is the specified size");
+MODULE_PARM_DESC(dstruct_size,"this is the specified size"); 
 
 //A//
 void constructLinkedList(void)
 {
+	int i;
 	LLhead = (struct Node *)kmalloc(sizeof(struct Node), GFP_KERNEL);
  	LLhead-> data = listData[0];
  	LLhead -> next = NULL;
  	LLcurNode = LLhead;
  	LLnewNode = NULL;
 
- 	int i = 1;
+ 	i = 1;
  	while(i < dstruct_size) 
  	{
  		LLnewNode = (struct Node *)kmalloc(sizeof(struct Node), GFP_KERNEL);
@@ -139,7 +145,8 @@ void enqueue(struct Node *toAdd)
 //Remove from front. returns NULL if the list is empty
 struct Node * dequeue(void)
 {
-	//printk("QcurNode is %i\n", QcurNode -> data);
+	struct Node *temp;
+	struct Node *rtn;
 
 	if(Qhead == NULL)
 	{
@@ -149,15 +156,12 @@ struct Node * dequeue(void)
 
 	QcurNode = Qhead;
 
-	struct Node *temp;
-	struct Node *rtn = (struct Node *)kmalloc(sizeof(struct Node), GFP_KERNEL);
+	rtn = (struct Node *)kmalloc(sizeof(struct Node), GFP_KERNEL);
 	rtn -> data = QcurNode -> data;
 	rtn -> next = QcurNode -> next;
 
 	temp = QcurNode -> next;
 	kfree(QcurNode);
-
-//	printk("QcurNode is NOW %i\n", QcurNode -> data);
 
 	Qhead = temp;
 
@@ -166,8 +170,10 @@ struct Node * dequeue(void)
 
 void printQueue(void)
 {
+	int i;
+
 	QcurNode = Qhead;
-	int i = 0;
+	i = 0;
  	while(NULL != QcurNode)
  	{
  		printk("The data is: %i at index %i\n", QcurNode->data, i);
@@ -179,7 +185,9 @@ void printQueue(void)
 //Constructs a Queue of dstruct_size elements of random values
 void constructQueue(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 	while(i < dstruct_size)
 	{
 		struct Node *toAdd = (struct Node *)kmalloc(sizeof(struct Node), GFP_KERNEL);
@@ -192,7 +200,9 @@ void constructQueue(void)
 
 void deconstructQueue(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 	while (NULL != Qhead)
 	{
 		printk("Just dequeued %i at index %i", dequeue() -> data, i);
@@ -261,7 +271,8 @@ int insertIntoMap(int toAdd)
 
 void displayMap(void)
 {
-	int i = 0;
+	int i;
+	i= 0;
 	while(i < UPPER_BOUND)
 	{
 		if(myMap->data[i] != NULL)
@@ -274,8 +285,9 @@ void displayMap(void)
 
 void constructMap(void)
 {
+	int i;
 	initializeMap();
-	int i = 0;
+	i = 0;
 	while(i < dstruct_size)
 	{	
 		insertIntoMap(mapData[i]);
@@ -307,50 +319,205 @@ void deconstructMap(void)
 	printk("The Map has been freed\n");
 }
 
+//Kernel docs provided information on RB Trees and inspiration for implementation
+//Returns NULL if fails to find.
+struct treeNode * searchInRBTree(struct rb_root *treeRoot, int toFind)
+{
+	struct rb_node *aNode = treeRoot->rb_node;
+
+	while(aNode != NULL)
+	{
+		struct treeNode *someNode = container_of(aNode, struct treeNode, node);
+		
+		if(toFind < someNode->data)
+		{
+			aNode = aNode->rb_left;
+		}
+		else if(toFind > someNode->data)
+		{
+			aNode = aNode->rb_right;
+		}
+		else
+		{
+			return someNode;
+		}
+	}
+	return NULL; //Failed to find.
+}
+
+int insertIntoTree(struct rb_root *treeRoot, struct treeNode *toAdd)
+{
+	struct rb_node **newTreeNode = &(treeRoot->rb_node);
+	struct rb_node *parent = NULL;
+
+	while(*newTreeNode != NULL)
+	{
+		struct treeNode *curTreeNode = container_of(*newTreeNode, struct treeNode, node);
+		parent = *newTreeNode;
+
+		if(curTreeNode->data < toAdd->data)
+		{
+			newTreeNode = &((*newTreeNode)->rb_left);
+		}
+		else if(curTreeNode->data > toAdd->data)
+		{
+			newTreeNode = &((*newTreeNode)->rb_right);
+		}
+		else
+		{	
+			return 0;
+		}
+	}
+
+	rb_link_node(&toAdd->node, parent, newTreeNode);
+	rb_insert_color(&toAdd->node, treeRoot);
+	return 1;
+}
+
+void displayTree(struct rb_root *treeRoot) 
+{
+    struct rb_node *cur = rb_first(treeRoot);
+
+    while(cur != NULL)
+     {
+        printk("The data of this node is: %i\n", rb_entry(cur, struct treeNode, node)->data);
+        cur = rb_next(cur);
+    }
+}
+
+void constructTree(void)
+{
+	int i;
+	i = 0;
+	while(i < dstruct_size)
+	{
+		struct treeNode *toAdd = (struct treeNode *)kmalloc(sizeof(struct treeNode), GFP_KERNEL);
+		toAdd->data = treeData[i];
+
+		if(searchInRBTree(&treeHead, treeData[i]) == NULL)
+		{
+			insertIntoTree(&treeHead, toAdd);
+		}
+		i++;
+	}
+}
+
+void removeRangeFromTree(struct rb_root *treeRoot)
+{
+	int treeRange[2];
+	int begin, end, i;
+
+	rangeData = treeRange;
+	get_random_bytes(rangeData, sizeof(treeRange));
+
+	if(treeRange[0] > treeRange[1])
+	{
+		begin = treeRange[1];
+		end = treeRange[0];
+	}
+	else
+	{
+		begin = treeRange[0];
+		end = treeRange[1];
+	}
+
+	i = begin;
+	if(begin < 0 && end < 0)
+	{
+		while(i > end)
+		{
+			struct treeNode *toCheck = searchInRBTree(treeRoot, i);
+			if(toCheck != NULL)
+			{
+				printk("Removing: %i\n", toCheck->data);
+				rb_erase(&toCheck->node, treeRoot);
+				kfree(toCheck);
+			}
+			i--;
+		}
+	}
+	else
+	{
+		while(i < end)
+		{
+			struct treeNode *toCheck = searchInRBTree(treeRoot, i);
+			if(toCheck != NULL)
+			{
+				printk("Removing: %i\n", toCheck->data);
+				rb_erase(&toCheck->node, treeRoot);
+				kfree(toCheck);
+			}
+			i++;
+		}
+	}
+	printk("Just removed elements between the range of: %i and %i\n", begin, end);
+}
+
+void deconstructTree(struct rb_node *treeRoot) 
+{
+	if(treeRoot != NULL)
+	{
+		deconstructTree(treeRoot->rb_left);
+   		deconstructTree(treeRoot->rb_right);
+    	kfree(treeRoot);
+	}
+	else
+	{
+		return;
+	}
+}
+
 
 static int lab2_module_init(void) 
 {
-	//printk("Queue\n----------------\n");
-
-	//printk("dstruct_size: %i\n", dstruct_size);
-
-	//int LLtemp[dstruct_size];
-	//int Qtemp[dstruct_size];
-	//int Mtemp[dstruct_size];
+	struct rb_node *treeFirst;
 	int Ttemp[dstruct_size];
-	//listData = LLtemp;
-	//queueData = Qtemp;
-	//mapData = Mtemp;
+	int LLtemp[dstruct_size];
+	int Qtemp[dstruct_size];
+	int Mtemp[dstruct_size];
+	printk("RBTree\n----------------\n");
+
+	listData = LLtemp;
+	queueData = Qtemp;
+	mapData = Mtemp;
 	treeData = Ttemp;
- 	//get_random_bytes(listData,sizeof(LLtemp));
- 	//get_random_bytes(queueData, sizeof(Qtemp));
- 	//get_random_bytes(mapData, sizeof(Mtemp));
+ 	get_random_bytes(listData,sizeof(LLtemp));
+ 	get_random_bytes(queueData, sizeof(Qtemp));
+ 	get_random_bytes(mapData, sizeof(Mtemp));
  	get_random_bytes(treeData,sizeof(Ttemp));
 
- 	//printk("----------------\n");
+ 	printk("----------------\n");
 
- 	/* Linked List
+ 	/* Linked List */
  	constructLinkedList();
  	printLinkedList();
- 	freeLinkedList();*/
+ 	freeLinkedList();
 
- 	/* Queue
+ 	/* Queue */
  	constructQueue();
  	printk("The queue is:\n");
  	printQueue();
- 	deconstructQueue();*/
+ 	deconstructQueue();
 
- 	/* Map
+ 	/* Map */
  	myMap = (struct Map *) kmalloc(sizeof(struct Map),  GFP_KERNEL);
  	myMap->size = 0;
  	constructMap();
  	printk("The Map is:\n");
  	displayMap();
- 	deconstructMap();*/
+ 	deconstructMap();
 
- 	/* Red and Black Tree*/
-
-
+ 	/* Red and Black Tree */
+ 	treeHead = RB_ROOT;
+ 	constructTree();
+ 	printk("The Tree is: \n");
+ 	displayTree(&treeHead);
+ 	removeRangeFromTree(&treeHead);
+ 	printk("The Tree is now: \n");
+ 	displayTree(&treeHead);
+ 	treeFirst = rb_first(&treeHead);  
+ 	deconstructTree(treeFirst);
+ 	printk("The Tree was freed\n");
 
  	return 0;
 }
